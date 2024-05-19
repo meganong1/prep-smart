fecb512df22f0c228816f62f4747a6b9524ba57e
qutip04 <leoqu2004@hotmail.com>
2024-05-18 18:23:36 -0700

    leo's branch + main

--- src/app/page.js
+++ src/app/page.js
@@ -60,6 +60,23 @@ export default function Page() {
           ))}
         </div>
       </div>
+
+      <main className={styles.main}>
+
+        <div className={styles.center}>
+        <img
+          className={styles.logo}
+          src="/PrepSmartLogo.png"
+          alt="PrepSmart Logo"
+          width={200}
+          height={100}
+        />
+        </div>
+        <div className={styles.container}>
+        <h1 className={styles.coolText}>PrepSmart</h1>
+        </div>
+      </main>
+
     </div>  
   );
 }
